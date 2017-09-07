5b1a2e1e592c17c9b284fd13bc82cad4b9c752f3
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-09-07 09:00:41 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+a=22
+print (a)
+print (a+5)
+print (a*3)
+print (a/2)
+print (a-2)
---
+++ test2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+name="jennifer"
+print (name)
+print (name*3)
+print ("My name is " + name)
+name1="Jennifer"
+name2=" J"
+print (name1+name2)
+print (name[0])
+print (name[1])
+print (name[3])
+print (name[7])
+print (name[8])
