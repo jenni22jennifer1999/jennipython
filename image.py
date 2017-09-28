e7fc94aecfea2f0223616de30e92d4af6d45d1ef
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-09-28 10:02:35 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import os
-import time
+import os  #importing os.
+import time   #importing time.
 a=0
 b=1
-while a<=10:
-    os.system("fswebcam -F 5 --fps 20 -r \"1200x800\".jpg")
+while a<=10:  #creating while loop.
+    os.system("fswebcam -F 5 --fps 20 -r \"1200x800\".jpg")  # capturing command.
     a=a+1
     b=b+1
-    time.sleep(30)
+    time.sleep(30)  # timings.
