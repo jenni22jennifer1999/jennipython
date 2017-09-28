fcf4fc35c23278925bad1e4bdebff52770f0979c
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-09-28 09:09:17 +0530

    Update jennimail.py

--- jennimail.py
+++ jennimail.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-#sending mail
-import smtplib
-fromaddr = '@gmail.com'
-toaddrs  = '@gmail.com'
-msg = 'hello!'
-username = '@gmail.com'
-password = ''
+#sending mail using python.
+import smtplib  #small message transfer protocol.
+fromaddr = '@gmail.com'    #sender mail id.
+toaddrs  = '@gmail.com'    #reciever mail id.
+msg = 'hello!'  #message to be sent.
+username = '@gmail.com'    #sender mail id.
+password = ''    #sender's password.
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
-server.quit()
+server.login(username,password)     #command to login.
+server.sendmail(fromaddr, toaddrs, msg)    #command to send the mail. 
+server.quit()   #exit. 
