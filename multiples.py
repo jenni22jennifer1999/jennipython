8e4951e631975a8857ca23f5720360c1c239f8a8
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-09-28 09:12:46 +0530

    Update multiples.py

--- multiples.py
+++ multiples.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-for i in range(1,21):
+#TABLES from 1 to 5.
+for i in range(1,21):  #2 tables.
 	if(i%2==0):
 		print(i)
-print("\n")
-for i in range(1,31):
+print("\n") 
+for i in range(1,31):   #3 tables.
 	if(i%3==0):
 		print(i)
 print("\n")
-for i in range(1,41):
+for i in range(1,41):   #4 tables.
 	if(i%4==0):
 		print(i)
 print("\n")
-for i in range(1,51):
+for i in range(1,51):   #5 tables.
 	if(i%5==0):
 		print(i)
 print("\n")
