23f617748c0700c2173f69e714c18f4ff9810553
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-10-09 15:17:10 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-a=22
-print (a)
-print (a+5)
-print (a*3)
-print (a/2)
-print (a-2)
+a=22 #initializing a number
+print (a)   #prints the value of a
+print (a+5) #addition
+print (a*3)  #multiplication
+print (a/2)  #division
+print (a-2)  #subtraction
