2ed6cd233049a28a1915bdc64f1387472dcabec7
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-10-09 15:21:16 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-name="jennifer"
-print (name)
-print (name*3)
-print ("My name is " + name)
+name="jennifer"  #initializing string func
+print (name)    #prints the value of name
+print (name*3)  #prints the value of name 3 times
+print ("My name is " + name)   #prints the sentence
 name1="Jennifer"
 name2=" J"
-print (name1+name2)
-print (name[0])
-print (name[1])
-print (name[3])
-print (name[7])
-print (name[8])
+print (name1+name2)  #prints Jennifer J
+print (name[0])    #prints  J (1st position)
+print (name[1])    #prints
+print (name[3])    #prints
+print (name[7])    #prints
+print (name[8])    #prints
