d21e769d57bc9e590d16e78f92da172374e8092b
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-11-10 14:03:21 +0530

    Update MotionSensor.py

--- MotionSensor.py
+++ MotionSensor.py
@@ -1,9 +1,9 @@
-from gpiozero import MotionSensor
-import os 
+from gpiozero import MotionSensor #importing motion sensor from gpiozero
+import os #importing os
 pir = MotionSensor(4)
-while True:
-    if pir.motion_detected:
-       r= print ("motion detected!")
+while True: #while condition
+    if pir.motion_detected:  #if statement
+       r= print ("motion detected!") #printing statement
        if pir=="r"
-        os.system("fswebcam -F 3 --fps 20 -r 1200x800 pir.jpg")
+        os.system("fswebcam -F 3 --fps 20 -r 1200x800 pir.jpg")  #image size and pixel
         
