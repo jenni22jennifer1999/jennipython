4ad33762adc83be36a2a4a8a405edba73c1261fe
jenni22jennifer1999 <31615198+jenni22jennifer1999@users.noreply.github.com>
2017-11-23 03:27:30 +0000

    Add files via upload

--- wir.py
+++ wir.py
@@ -1,14 +1,23 @@
 import wiringpi
-wiringpi.pinMode(19,INPUT) 
-#wiringPiSetupGpio()
-pinMode(GAS_PIN,INPUT)
+import RPi.GPIO as GPIO
+import time
 
-while(1):
-    if(digitalRead(GAS_PIN)==LOW):
+GAS_PIN = 19
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GAS_PIN,GPIO.IN)
+
+
+while 1:
+#    print ("Checking ...")
+    val = GPIO.input(GAS_PIN)
+
+    time.sleep(1)
+
+#    print (val)
+
+    if val==0:
         print("GAS detected")
         print("\n")
     else:
         print("Cool environment")
         print("\n")
-    delay(2000)
-  
